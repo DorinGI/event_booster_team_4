c281e8c417f7c60b9b57a6d326d293bafbd44ad7
DorinGI <158717197+DorinGI@users.noreply.github.com>
2024-08-04 20:01:49 +0200

    Start Config

--- src/index.js
+++ src/index.js
@@ -1,72 +1,6 @@
 import EventsApi from './api.js';
 
-// const window = document.defaultView;
-
 const eventsApi = new EventsApi();
-
-// window.addEventListener('load', onSubmit);
-
-// function onSubmit(e) {
-//     e.preventDefault();
-
-//     const window = document.defaultView;
-//     console.log(window);
-//     fetchEvents();
-// }
-
-// function fetchEvents() {
-//     return eventsApi
-//         .getEvents()
-//         .then(({ events }) => {
-//             if (events?.length === 0) {
-//                 throw new Error("No Data")
-//             }
-//             return events.reduce(
-//                 (markup, event) => createMarkup(event) + markup,
-//                 ""
-//             );
-//         })
-//         .then((markup) => {
-//         updateEventsList(markup);
-//         })
-//         .catch(onError);
-// }
-// function createMarkup(event) {
-//     const { name,url} = event;
-
-//     return `
-//      <div class="event-card">
-//      <img src= class="event-img"> 
-//      <h2 class="event-title">${name}</h2>
-//      <h3 class="event-date"></h3>
-//      <p class="event-locations"></p>
-//      <a href=${url} class="event-link" target="_blank">Read more</a>
-//     </div>
-//     `;
-// }
-
-// function updateEventsList(markup) {
-//   document
-//     .querySelector('.cards')
-//     .insertAdjacentHTML("beforeend", markup);
-// }
-// function onError(err) {
-//   console.error(err);
-// }
-
-// GPT
-// const apiKey = 'Z9sML3GkU2JtjpwYuKAphTWzMdRrsxCG';
-// const apiUrl = `https://app.ticketmaster.com/discovery/v2/events?apikey=${apiKey}&pageSize=16&page=1`;
-
-// function fetchEvents() {
-//     return fetch(apiUrl)
-//         .then(response => {
-//             if (!response.ok) {
-//                 throw new Error('Network response was not ok');
-//             }
-//             return response.json();
-//         });
-// }
 
 function displayEvents(events) {
     const cardsContainer = document.querySelector('.cards');
@@ -74,10 +8,10 @@ function displayEvents(events) {
         const card = document.createElement('div');
         card.className = 'card';
         card.innerHTML = `
-            <img src="${event.images[0].url}" alt="${event.name}">
-            <h3>${event.name}</h3>
-            <p>${event.dates.start.localDate}</p>
-            <p>${event._embedded.venues[0].name}</p>
+            <img class="event-image" src="${event.images[0].url}" alt="${event.name}">
+            <h3 class="event-name">${event.name}</h3>
+            <p class="event-date">${event.dates.start.localDate}</p>
+            <p class="event-place">${event._embedded.venues[0].name}</p>
                 `;
         cardsContainer.appendChild(card);
     });
