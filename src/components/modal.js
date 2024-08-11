0cebe4b490cfcc768bd4dae99a33514d8ce39713
DorinGI <158717197+DorinGI@users.noreply.github.com>
2024-08-11 15:53:52 +0200

    Modal Design responsive

--- src/components/modal.js
+++ src/components/modal.js
@@ -1,7 +1,10 @@
 export function openModal(event) {
   const modal = document.getElementById('eventModal');
   const modalBody = document.getElementById('modal-body');
-  modalBody.innerHTML = `
+
+  if (event.priceRanges && event.priceRanges.length == 1) {
+    // Modalul fara VIP
+    modalBody.innerHTML = `
   <style>
         .modal-logo {
             background-image: url('${event.images[1].url}');
@@ -26,16 +29,99 @@ export function openModal(event) {
           <h2 class="modal-title">Who</h2>
           <p class="modal-text">${event.name}</p>
           <h2 class="modal-title">Prices</h2>
-          <p class="modal-text">${event.priceRanges[0].type || ''} ${
-    event.priceRanges[0].min || ''
-  }-${event.priceRanges[0].max || ''} ${event.priceRanges[0].currency || ''}</p>
-          <div class="button-grup">            
-          <button class="button-standard">BUY TICKETS</button>
-          <button class="button-vip">BUY TICKETS</button>
-        </div> 
-      <div>
+          <div class="button-grup"> 
+            <div>   
+              <p class="modal-text price">${event.priceRanges[0].type || ''} ${
+      event.priceRanges[0].min || ''
+    }-${event.priceRanges[0].max || ''} ${
+      event.priceRanges[0].currency || ''
+    }</p>
+              <button class="button-standard">BUY TICKETS</button>
+            </div> 
+          </div> 
+        <div>
       <button class="button-more">MORE FROM THIS AUTOR</button>
       <div id="more-events-container"></div>`;
+  } else if (event.priceRanges && event.priceRanges.length > 1) {
+    // modalul pentru VIP
+    modalBody.innerHTML = `
+  <style>
+        .modal-logo {
+            background-image: url('${event.images[1].url}');
+        }
+        .modal-image {
+            background-image: url('${event.images[5].url}');
+        }
+    </style>    
+      <div class="modal-logo"></div> 
+      <div class="modal-main">             
+        <div class="modal-image"></div> 
+        <div class="modal-main-text">
+          <h2 class="modal-title">Info</h2>          
+          <div class="modal-text">${event.info || ''}</div>
+            <h2 class="modal-title">When</h2>
+            <p class="modal-text">${event.dates.start.localDate}</p>
+            <h2 class="modal-title">Where</h2>
+            <p class="modal-text">${event._embedded.venues[0].name}</p>
+          </div>
+        </div>
+        <div class="modal-buy">
+          <h2 class="modal-title">Who</h2>
+          <p class="modal-text">${event.name}</p>
+          <h2 class="modal-title">Prices</h2>
+          <div class="button-grup">  
+            <div> 
+              <p class="modal-text price">${event.priceRanges[0].type || ''} ${
+      event.priceRanges[0].min || ''
+    }-${event.priceRanges[0].max || ''} ${
+      event.priceRanges[0].currency || ''
+    }</p>
+              <button class="button-standard">BUY TICKETS</button>
+            </div> 
+            <div>
+              <p class="modal-text price">${event.priceRanges[1].type || ''} ${
+      event.priceRanges[1].min || ''
+    }-${event.priceRanges[1].max || ''} ${
+      event.priceRanges[1].currency || ''
+    }</p>
+              <button class="button-vip">BUY TICKETS</button>
+            </div> 
+          </div> 
+        <div>
+      <button class="button-more">MORE FROM THIS AUTOR</button>
+      <div id="more-events-container"></div>`;
+  } else {
+    // Modalul fara Price
+    modalBody.innerHTML = `
+  <style>
+        .modal-logo {
+            background-image: url('${event.images[1].url}');
+        }
+        .modal-image {
+            background-image: url('${event.images[5].url}');
+        }
+    </style>    
+      <div class="modal-logo"></div> 
+      <div class="modal-main">             
+        <div class="modal-image"></div> 
+        <div class="modal-main-text">
+          <h2 class="modal-title">Info</h2>          
+          <div class="modal-text">${event.info || ''}</div>
+            <h2 class="modal-title">When</h2>
+            <p class="modal-text">${event.dates.start.localDate}</p>
+            <h2 class="modal-title">Where</h2>
+            <p class="modal-text">${event._embedded.venues[0].name}</p>
+          </div>
+        </div>
+        <div class="modal-buy">
+          <h2 class="modal-title">Who</h2>
+          <p class="modal-text">${event.name}</p>
+          <h2 class="modal-title">Prices</h2>
+          <p class="modal-text">No tickets available .</p>
+        </div>
+      <button class="button-more">MORE FROM THIS AUTOR</button>
+      <div id="more-events-container"></div>`;
+  }
 
   modal.style.display = 'flex';
 
